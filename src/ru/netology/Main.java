3f2b5ec3777617b10dd1760658ba91f78f5a87c3
alexcc0a <alex93nest@mail.ru>
2022-03-22 21:39:52 +0600

    Inicial

---
+++ src/ru/netology/Main.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ThreadGroup mainGroup = new ThreadGroup("main group");
+        System.out.println("Создаю потоки...");
+        Thread thread1 = new MyThread(mainGroup, "Поток №1");
+        Thread thread2 = new MyThread(mainGroup, "Поток №2");
+        Thread thread3 = new MyThread(mainGroup, "Поток №3");
+        Thread thread4 = new MyThread(mainGroup, "Поток №4");
+
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Завершаю потоки!");
+        mainGroup.interrupt();
+    }
+}
---
+++ src/ru/netology/MyThread.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+class MyThread extends Thread {
+
+    public MyThread(ThreadGroup group, String name) {
+        super(group, name);
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while(!isInterrupted()) {
+                Thread.sleep(2500);
+                System.out.println("Всем привет!");
+            }
+        } catch (InterruptedException err) {
+
+        } finally{
+            System.out.printf("%s завершен\n", getName());
+        }
+    }
+}
